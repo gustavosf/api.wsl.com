606769eb5c452f36cded896d1e645dbca2ae14cd
Gustavo Seganfredo <gustavosf@gmail.com>
2012-04-02 09:14:58 -0300

    Skeleton code created

---
+++ api.wsl.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace wsl;
+
+class WSL {
+
+	private $accessCode;
+
+	public function __construct($accessCode)
+	{
+		$this->accessCode = $accessCode;
+	}
+
+}
+
+class CardioTrainer {
+
+	private $accessCode;
+
+	public function __construct($accessCode)
+	{
+		$this->accessCode = $accessCode;
+	}
+
+}
+
+class Calorific {
+
+	private $accessCode;
+
+	public function __construct($accessCode)
+	{
+		$this->accessCode = $accessCode;
+	}
+
+}
