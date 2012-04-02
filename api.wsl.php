0d28ebfa098e495a3944fe7b981e491fb68af318
Gustavo Seganfredo <gustavosf@gmail.com>
2012-04-02 09:20:14 -0300

    included getters for wsl modules

--- api.wsl.php
+++ api.wsl.php
@@ -5,10 +5,29 @@ namespace wsl;
 class WSL {
 
 	private $accessCode;
+	private $modules = array();
 
 	public function __construct($accessCode)
 	{
 		$this->accessCode = $accessCode;
+	}
+
+	public function cardioTrainer()
+	{
+		if (!isset($this->modules['cardioTrainer']))
+		{
+			$this->modules['cardioTrainer'] = new CardioTrainer($this->accessCode);
+		}
+		return $this->modules['cardioTrainer'];
+	}
+
+	public function calorific()
+	{
+		if (!isset($this->modules['calorific']))
+		{
+			$this->modules['calorific'] = new Calorific($this->accessCode);
+		}
+		return $this->modules['calorific'];
 	}
 
 }
