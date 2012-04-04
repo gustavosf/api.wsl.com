51229f7000f57a96a5ba0febdd29b5bfcfa814b2
Gustavo Seganfredo <gustavosf@gmail.com>
2012-04-03 23:21:41 -0300

    Implemented some kind of cache. Not ideal though

--- src/wsl.module.php
+++ src/wsl.module.php
@@ -9,8 +9,10 @@ if (!function_exists('json_decode'))
 
 class Module {
 
-	protected $data;
-	protected $ws_uri = 'http://www.worksmartlabs.com/servlets/HighScoreServer/';
+	private $data;
+	private $ws_uri = 'http://www.worksmartlabs.com/servlets/HighScoreServer/';
+	private $cache_path = '/tmp/';
+	private $cache_time_to_expire = 10800; /* 3 hours */
 
 	/**
 	 * Retrieves data from wsl webservice
@@ -20,10 +22,20 @@ class Module {
 	{
 		if (!isset($this->data[$service]))
 		{
-			$request = new \HttpRequest($this->ws_uri.$service.'.htm', \HttpRequest::METH_POST);
-			$request->setPostFields($params);
-			$request->send();
-			$this->data = json_decode($request->getResponseBody());
+			/* cache file must include a uniqueid for the user (accesscode maybe?) */
+			$cache_file = $this->cache_path.str_replace('/','_',$service).'.cache';
+			if (time() - @filemtime($cache_file) > $this->cache_time_to_expire)
+			{
+				$request = new \HttpRequest($this->ws_uri.$service.'.htm', \HttpRequest::METH_POST);
+				$request->setPostFields($params);
+				$request->send();
+				$this->data = json_decode($request->getResponseBody());
+				file_put_contents($cache_file, serialize($this->data));
+			}
+			else
+			{
+				$this->data = unserialize(file_get_contents($cache_file));
+			}
 		}
 		return $this->data;
 	}
